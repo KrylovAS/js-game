2df5c02da35ab94eda25c67ba439941ca32e7bd4
KrylovAS <s1nii@mail.ru>
2018-03-22 23:24:39 +0400

    тест

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
 'use strict';
+
+class Vector {
+    constructor(x = 0, y = 0) {        
+        this.x = x;
+        this.y = y;
+    }
+    plus(vector) {
+        if(vector instanceof Vector ) {
+            return  new Vector(this.x + vector.x , this.y + vector.y )
+        }else {            
+            throw new Error('Передан другой класс');
+        };
+    }
+    times(n) {
+        console.log(this.x)
+        return new Vector(this.x * n, this.y * n)
+    }
+
+}
+
+
