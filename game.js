5c0b1f99ce805028eb398e5e1615e5e9105c709c
KrylovAS <s1nii@mail.ru>
2018-03-24 23:07:25 +0400

    реализованы свойства класса Actor

--- game.js
+++ game.js
@@ -9,14 +9,53 @@ class Vector {
         if(vector instanceof Vector ) {
             return  new Vector(this.x + vector.x , this.y + vector.y )
         }else {            
-            throw new Error('Передан не класс Vector');
+          throw new Error('Можно прибавлять к вектору только вектор типа Vector');
         };
     }
-    times(n) {
-        console.log(this.x)
-        return new Vector(this.x * n, this.y * n)
+    times(n) {        
+        return new Vector(this.x * n, this.y * n);
+    }
+}
+
+class Actor {
+    constructor(pos = new Vector(0,0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+        if(!(pos instanceof Vector ) || !(size instanceof Vector) || !(speed instanceof Vector)) {
+            throw new Error()        
+        }
+        this.pos = pos;
+        this.size = size;
+        this.speed = speed;
+    
+        Object.defineProperties(this, {
+            'left': {
+                value: this.pos.x
+            },
+            'top': {
+                value: this.pos.y
+            },
+            'right': {
+                value: this.pos.x + this.size.x
+            },
+            'bottom': {
+                value: this.pos.y + this.size.x
+            },
+            'type': {
+                value: 'actor'
+            }
+        })
+    }
+    
+    act() {
+    
+    }
+    isIntersect(Actor) {
+        if(!(actor instanceof Actor)){
+          throw new Error()  
+        }
+
+
     }
 
 }
-
+console.log('gggg')
 
